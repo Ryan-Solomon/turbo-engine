00530d4a092fc66a883edc8c2e02effe1d34c3a9
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-08 11:25:51 -0600

    setup form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export const Form = () => {
-  return <h1>Form</h1>;
+  return (
+    <>
+      <SForm>
+        <FormSection>
+          <Label id='name'>Full Name</Label>
+          <Input type='text' name='name' />
+        </FormSection>
+        <FormSection>
+          <Label id='email'>Email</Label>
+          <Input type='email' name='email' />
+        </FormSection>
+        <FormSection>
+          <Label id='password'>Password</Label>
+          <Input type='password' name='password' />
+        </FormSection>
+      </SForm>
+    </>
+  );
 };
+
+const SForm = styled.form``;
+
+const FormSection = styled.section``;
+
+const Label = styled.label``;
+
+const Input = styled.input``;
