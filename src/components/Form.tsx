88c24d3705d0fd56169e6067ff3b14bbca497560
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-08 11:32:49 -0600

    add state to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styled from 'styled-components';
 
+type TFormState = {
+  fullName: string;
+  email: string;
+  password: string;
+};
+
 export const Form = () => {
+  const [formState, setFormState] = useState<TFormState>({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+
+  function updateForm(
+    event: ChangeEvent<HTMLInputElement>,
+    kind: 'name' | 'password' | 'email'
+  ) {
+    if (kind === 'name') {
+      setFormState({ ...formState, fullName: event.target.value });
+    } else if (kind === 'password') {
+      setFormState({ ...formState, password: event.target.value });
+    } else if (kind === 'email') {
+      setFormState({ ...formState, email: event.target.value });
+    } else {
+      throw new Error('kind not supported');
+    }
+  }
+
   return (
     <>
       <SForm>
         <FormSection>
           <Label id='name'>Full Name</Label>
-          <Input type='text' name='name' />
+          <Input
+            value={formState.fullName}
+            onChange={(e) => updateForm(e, 'name')}
+            type='text'
+            name='name'
+          />
         </FormSection>
         <FormSection>
           <Label id='email'>Email</Label>
-          <Input type='email' name='email' />
+          <Input
+            value={formState.email}
+            onChange={(e) => updateForm(e, 'email')}
+            type='email'
+            name='email'
+          />
         </FormSection>
         <FormSection>
           <Label id='password'>Password</Label>
-          <Input type='password' name='password' />
+          <Input
+            value={formState.password}
+            onChange={(e) => updateForm(e, 'password')}
+            type='password'
+            name='password'
+          />
         </FormSection>
       </SForm>
     </>
