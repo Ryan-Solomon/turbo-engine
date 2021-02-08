336f8670079f8cc29c4ee10e4de0612ad525f8db
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-08 13:23:04 -0600

    add form tests

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Form } from './Form';
 
@@ -6,7 +7,21 @@ describe('Form tests', () => {
   test('render form', () => {
     render(<Form />);
     expect(screen.getByText(/submit/i)).toBeInTheDocument();
-    screen.debug();
+  });
+  test('form input works', () => {
+    render(<Form />);
+    const [nameInput, emailInput, passwordInput] = screen.getAllByRole(
+      'textbox'
+    );
+    const testName = 'joe smith';
+    const testEmail = 'joe@smith.com';
+    const testPassword = 'joesmithjoe24!';
+    userEvent.type(nameInput, testName);
+    userEvent.type(emailInput, testEmail);
+    userEvent.type(passwordInput, testPassword);
+    expect(screen.getByText(testName)).toBeInTheDocument();
+    expect(screen.getByText(testEmail)).toBeInTheDocument();
+    expect(screen.getByText(testPassword)).toBeInTheDocument();
   });
 });
 
