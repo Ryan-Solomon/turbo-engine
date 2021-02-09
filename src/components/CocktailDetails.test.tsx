60e2ce3a05545e190ae0b1277999b2c70ca572f2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-09 17:34:07 -0600

    add test for cocktail details

--- src/components/CocktailDetails.test.tsx
+++ src/components/CocktailDetails.test.tsx
@@ -1,10 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import React from 'react';
 import { CocktailDetails } from './CocktailDetails';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Cocktail details page', () => {
-  test('renders', () => {
+  test('renders', async () => {
     render(<CocktailDetails />, { wrapper: MemoryRouter });
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+    expect(screen.getByText(/failed to load/i)).toBeInTheDocument();
   });
 });
