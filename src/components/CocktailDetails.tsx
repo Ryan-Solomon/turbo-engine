42df5f1baeca6481008288e81f3caf2f499610b7
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-09 16:41:32 -0600

    add styled components

--- src/components/CocktailDetails.tsx
+++ src/components/CocktailDetails.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { TCocktail } from './Cocktails';
+import styled from 'styled-components';
 
 type TProps = {
   cocktail: TCocktail;
@@ -7,5 +8,18 @@ type TProps = {
 
 export const CocktailDetails: FC<TProps> = ({ cocktail }) => {
   const { strDrink, strDrinkThumb, strCategory, strInstructions } = cocktail;
-  return <h1>Coacktail Details</h1>;
+  return <Container></Container>;
 };
+
+const Container = styled.div``;
+
+const Image = styled.img`
+  height: 250px;
+  width: 250px;
+  object-fit: cover;
+`;
+const Instructions = styled.p``;
+
+const Category = styled.h3``;
+
+const Title = styled.h2``;
