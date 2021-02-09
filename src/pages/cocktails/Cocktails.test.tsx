b8387691be13940f910a6524e739eeb7648a6d50
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-09 16:13:19 -0600

    wait for loading to be removed

--- src/pages/cocktails/Cocktails.test.tsx
+++ src/pages/cocktails/Cocktails.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {
+  queryByText,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import { CocktailsPage } from './CocktailsPage';
 
 describe('Cocktails page', () => {
-  test('Renders and loads images', () => {
+  test('Renders and loads images', async () => {
     render(<CocktailsPage />);
     expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
     screen.debug();
   });
 });
