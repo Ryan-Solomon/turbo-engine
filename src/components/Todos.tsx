e7b92421474aadca5bcfc3c4d9fe3b5e24ef825c
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-09 19:23:54 -0600

    add todos

---
+++ src/components/Todos.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Todos = () => {
+  return <h1>Todos</h1>;
+};
---
+++ src/pages/todos/TodoPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Todos } from '../../components/Todos';
+
+export const TodoPage = () => {
+  return (
+    <>
+      <Todos />
+    </>
+  );
+};
