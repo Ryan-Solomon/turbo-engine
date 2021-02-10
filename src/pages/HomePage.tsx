fead9bfb87370f19f410d1f31d65f44f5c129b4b
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-09 19:22:07 -0600

    add bg to home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import img from '../assets/images/skyline.jpg';
 
 export const HomePage = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <Container>
+      <BackgroundImage src={img} alt='background-image' />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 92vh;
+  width: 100%;
+`;
+
+const BackgroundImage = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
